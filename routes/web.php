2c9faec8635d1224164e6a05d6f18614936fe737
Fidelcom <okpara2040@gmail.com>
2023-09-13 18:45:53 +0100

    Admin dashbord setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Analyst\AnalystController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\AdminProfileController;
+use App\Http\Controllers\Subadmin\SubAdminController;
+use App\Http\Controllers\Vendor\VendorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +22,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+//Admin
+Route::get('/admin/login', [AdminController::class, 'login'])->name('admin.login');
+Route::middleware('auth', 'role:admin')->prefix('/admin')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+    Route::get('/logout', [AdminController::class, 'destroy'])->name('admin.logout');
+
+    //Profile
+    Route::get('/profile/edit', [AdminProfileController::class, 'edit'])->name('admin.profile');
+    Route::post('/profile/update', [AdminProfileController::class, 'update'])->name('admin.profile.update');
+    Route::get('/profile/change/password', [AdminProfileController::class, 'show'])->name('admin.change.password');
+    Route::post('/profile/change/password/store', [AdminProfileController::class, 'store'])->name('admin.change.password.store');
+});
+
+//Vendor
+Route::middleware('auth', 'role:vendor')->prefix('/vendor')->group(function () {
+    Route::get('/dashboard', [VendorController::class, 'index']);
+});
+
+//Sub-admin
+Route::middleware('auth', 'role:subadmin')->prefix('/sub-admin')->group(function () {
+    Route::get('/dashboard', [SubAdminController::class, 'index']);
+});
+
+// Analyst
+Route::middleware('auth', 'role:analyst')->prefix('/analyst')->group(function () {
+    Route::get('/dashboard', [AnalystController::class, 'index']);
+});
+
+require __DIR__.'/auth.php';
